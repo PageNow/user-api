7b7142c1aed276504a9e02e9203540bebff1d240
Yongkyun Lee <yongkyun.daniel.lee@gmail.com>
2021-12-11 01:55:44 -0500

    Update nat gateway availability zone

--- terraform/02_network.tf
+++ terraform/02_network.tf
@@ -57,11 +57,18 @@ resource "aws_route_table" "public-route-table" {
         Name = "public-route-table"
     }
 }
-resource "aws_route_table" "private-route-table" {
+resource "aws_route_table" "private-route-table-1" {
     vpc_id = aws_vpc.production-vpc.id
 
     tags = {
-        Name = "private-route-table"
+        Name = "private-route-table-1"
+    }
+}
+resource "aws_route_table" "private-route-table-2" {
+    vpc_id = aws_vpc.production-vpc.id
+
+    tags = {
+        Name = "private-route-table-2"
     }
 }
 
@@ -75,30 +82,45 @@ resource "aws_route_table_association" "public-route-2-association" {
     subnet_id      = aws_subnet.public-subnet-2.id
 }
 resource "aws_route_table_association" "private-route-1-association" {
-    route_table_id = aws_route_table.private-route-table.id
+    route_table_id = aws_route_table.private-route-table-1.id
     subnet_id      = aws_subnet.private-subnet-1.id
 }
 resource "aws_route_table_association" "private-route-2-association" {
-    route_table_id = aws_route_table.private-route-table.id
+    route_table_id = aws_route_table.private-route-table-2.id
     subnet_id      = aws_subnet.private-subnet-2.id
 }
 
 # Elastic IP
-resource "aws_eip" "elastic-ip-for-nat-gw" {
+resource "aws_eip" "elastic-ip-for-nat-gw-1" {
     vpc                       = true
     associate_with_private_ip = "10.0.0.11"
     depends_on                = [aws_internet_gateway.production-igw]
 }
+resource "aws_eip" "elastic-ip-for-nat-gw-2" {
+    vpc                       = true
+    associate_with_private_ip = "10.0.0.12"
+    depends_on                = [aws_internet_gateway.production-igw]
+}
 
 # NAT gateway
-resource "aws_nat_gateway" "nat-gw" {
-    allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+resource "aws_nat_gateway" "nat-gw-1" {
+    allocation_id = aws_eip.elastic-ip-for-nat-gw-1.id
     subnet_id     = aws_subnet.public-subnet-1.id
-    depends_on    = [aws_eip.elastic-ip-for-nat-gw]
+    depends_on    = [aws_eip.elastic-ip-for-nat-gw-1]
 }
-resource "aws_route" "nat-gw-route" {
-    route_table_id         = aws_route_table.private-route-table.id
-    nat_gateway_id         = aws_nat_gateway.nat-gw.id
+resource "aws_route" "nat-gw-route-1" {
+    route_table_id         = aws_route_table.private-route-table-1.id
+    nat_gateway_id         = aws_nat_gateway.nat-gw-1.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+resource "aws_nat_gateway" "nat-gw-2" {
+    allocation_id = aws_eip.elastic-ip-for-nat-gw-2.id
+    subnet_id     = aws_subnet.public-subnet-2.id
+    depends_on    = [aws_eip.elastic-ip-for-nat-gw-2]
+}
+resource "aws_route" "nat-gw-route-2" {
+    route_table_id         = aws_route_table.private-route-table-2.id
+    nat_gateway_id         = aws_nat_gateway.nat-gw-2.id
     destination_cidr_block = "0.0.0.0/0"
 }
 
